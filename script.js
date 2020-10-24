c559f8a7d54a0bdf2f67de661d73b4d0f7dbff83
Andrew Olson <frrrkin@gmail.com>
2020-10-23 21:07:03 -0700

    set up test structure to support asserts

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var generateBtn = null;
+
+function isNode()
+{
+  return ((typeof process) === 'object');
+}
+
+function assert(x)
+{
+  if(!x)
+  {
+    throw "FAIL: " + x + " isn't true.  And UR ugly.";
+  }
+}
+
+// main
+if(!isNode()) {
+  generateBtn = document.querySelector("#generate");
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+}
+else
+{
+  // node main
+  console.log("Node is running.  If the script just completes silently then nothing below blew up.");
+  
+  assert(aFunctioinWithNoRuntimeBrowserDependencies_ThisJustDoublesTheArgument(5) == 10);
+}
+
+function aFunctioinWithNoRuntimeBrowserDependencies_ThisJustDoublesTheArgument(x)
+{
+  return x * 2;
+}
+
+function generatePassword()
+{
+  return "fakepassword - doesn't work yet";
+}
